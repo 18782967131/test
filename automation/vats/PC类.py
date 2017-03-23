bd203e440f0cd3671f806885716da4a0304dc82b
18782967131 <515947075@qq.com>
2017-03-23 16:01:37 +0800

    change pc2

--- automation/vats/PC类.py
+++ automation/vats/PC类.py
@@ -24,6 +24,411 @@ from vautils import logger
 from ipaddress import AddressValueError, NetmaskValueError
 from vautils.config.linux import ConfigUtil
 from time import gmtime, strftime
+
+"""coding: utf-8
+
+Copyright 2016 vArmour Networks private.
+All Rights reserved. Confidential
+
+Client asbtracts the client side functionality of a paramiko SSH session
+
+.. moduleauthor:: ppenumarthy@varmour.com
+"""
+
+import paramiko
+import socket
+import time
+
+from access import logger
+from access.exceptions import ConnectFail, ChannelFail, AuthFail
+
+
+class Client(object):
+    """
+    Client implements the client side functionality of an SSH session
+    """
+    def __init__(
+        self,
+        host=None,
+        port=22,
+        user='varmour',
+        password='vArmour123',
+        debug=None
+    ):
+        """
+        Initiates the SSH Client object
+
+        Kwargs:
+            :host (str): ip address of the host (SSH server)
+            :port (int): SSH service port
+            :user (str): linux user initiating the SSH session
+            :password (str): login password for the user
+        """
+        self._host = host
+        self._port = port
+        self._user = user
+        self._password = password
+        self._debug = debug
+
+        self._transport = None
+        self._log = logger
+
+    def connect(self, persistent=False, retry_timeout=60):
+        """
+        Creates a paramiko transport object, negotiates an SSH2 session as
+        a client, and authenticates the user with the credentials passed
+        in to the Client object
+
+        Raises:
+            :SSHException: On connection failure, or an SSH negotiation\
+                failure. This exception is re raised through ConnectFail\
+                exception.
+        """
+        retry = True
+        end_time = time.time() + retry_timeout
+        while retry:
+            if time.time() < end_time:
+                try:
+                    transport = paramiko.Transport((self._host, self._port))
+                    self.set_transport(transport)
+                    transport.start_client()
+                    authd = self._authenticate()
+                except (paramiko.ssh_exception.SSHException,
+                        socket.timeout) as e:
+                    self.disconnect()
+                    if persistent:
+                        if self._debug:
+                            self._log.info("retrying connect..")
+                    else:
+                        retry = False
+                        raise ConnectFail(e)
+                else:
+                    retry = False
+                    if authd and self._debug:
+                        self._log.debug(
+                            "user '{}' successfully initiated an SSH session to {}"
+                            .format(self._user, self._host)
+                        )
+            else:
+                retry = False
+                self._log.error("connect timed out!")
+
+    def disconnect(self):
+        """
+        Closes the current SSH2 session and any open channels associated
+        with it.
+        """
+        transport = self.get_transport()
+        if transport:
+            if self._debug:
+                self._log.debug(
+                    "closing SSH session and open transport channels to {}"
+                    .format(self._host)
+                )
+            transport.close()
+
+    def connected(self):
+        """
+        Checks whether the SSH2 session that was started is active
+
+        Returns:
+            :True|False (bool): True if session is active (if not False)
+        """
+        transport = self.get_transport()
+        if transport:
+            return transport.is_active()
+        else:
+            if self._debug:
+                self._log.debug("Transport disconnected!")
+            return False
+
+    def open_channel(self, term='console', height=24, width=80):
+        """
+        Request the server to open a channel - if successful, a pseudo
+        terminal with a specified height, and width is requested - if
+        successfull an interactive shell is requested. The stderr and
+        stdout streams will be combined, so that errors will appear in
+        stdout and thus in recv calls (otherwise errors have to be
+        explicitly received through recv_stderr and recv_stderr_ready)
+
+        Kwargs:
+            :term (str): terminal type
+            :height (int): height of the terminal (default 24 characters)
+            :width (int): width of the terminal (default 80 characters)
+
+        Returns:
+            :channel: a new channel will be connected to the stdin and
+                stdout of the pty
+
+        Raises:
+            :SShException: if any of the requests are rejected or the
+                session ends prematurely. This exception is re raised
+                through ChannelFail exception.
+        """
+        if self.connected():
+            try:
+                channel = self._transport.open_session()
+                channel.get_pty(term, width, height)
+                channel.invoke_shell()
+                channel.set_combine_stderr(True)
+            except paramiko.ssh_exception.SSHException as e:
+                self.disconnect()
+                raise ChannelFail(e)
+            else:
+                if self._debug:
+                    self._log.debug(
+                        "Transport channel created to {}".format(self._host)
+                    )
+                return channel
+
+    def get_transport(self):
+        """
+        Get the transport attribute of the session
+        """
+        return self._transport
+
+    def set_transport(self, transport):
+        """
+        Set the transport attribute to the instantiated paramiko transport
+        object
+
+        Args:
+            :transport (paramiko.Transport): Instance of the paramiko
+                Transport object
+        """
+        self._transport = transport
+
+    def _authenticate(self):
+        """
+        Helper method to authenticate a user - calls the auth_password
+        instance method of the paramiko.Transport object.
+
+        Raises:
+            :AuthenticationException: On invalid user name and password
+            :BadAuthenticationType: If the authentication scheme is not
+            supported by the server.
+            One of the above exceptions is re raised through the AuthFail
+            exception.
+        """
+        try:
+            transport = self.get_transport()
+            transport.auth_password(
+                    self._user, self._password, fallback=True
+            )
+        except (paramiko.ssh_exception.AuthenticationException,
+                paramiko.ssh_exception.BadAuthenticationType) as e:
+            self.disconnect()
+            raise AuthFail(e, user=self._user, password=self._password)
+        else:
+            return True
+
+            
+class LinuxShell(object):
+    """
+    Class implements access to shell (bash).
+    """
+    def __init__(
+        self,
+        host=None,
+        port=22,
+        user=None,
+        pswd=None,
+        debug=None
+    ):
+        """
+        Initializes the shell access object
+
+        Kwargs:
+            :version (str): version of BD product
+            :controller_ip (IPv4Address): ip address of the controller
+            :service_port (str): SSL service port
+            :user (str): user name of a valid bd shell user
+            :password (str): valid password of the BD user
+        """
+        self._host = host
+        self._port = port
+        self._user = user
+        self._password = pswd
+        self._debug = debug
+        self._log = logger
+
+        self._client = Client(host, port, user, pswd, debug)
+
+    def exec_command(
+        self,
+        command=None,
+        timeout=60,
+        output_exp=None,
+        error_exp=True,
+        exit_info=None,
+        retry_count=3,
+        retry_delay=5
+    ):
+        """
+        Method to execute a shell command remotely - it connects the created
+        client instance, and calls the helper _exec_shell_command.
+
+        Kwargs
+            :commmand (str): linux command as a string
+            :timeout (int):
+            :output_exp (str):
+            :error_exp (str):
+            :exit_info:
+            :retry_count (int):
+            :retry_delay (int):
+
+        Returns:
+            :command output as a list of lines
+        """
+        if not self._client.connected():
+            self._client.connect()
+
+        output, exit_status = self._exec_shell_command(
+                                  command,
+                                  timeout=timeout,
+                                  retry_count=retry_count,
+                                  retry_delay=retry_delay
+                              )
+
+        if isinstance(exit_info, dict):
+            exit_info['status'] = exit_status
+
+        if exit_status != 0:
+            self._log.warn("command could not be executed as expected: {}"
+                           .format(output))
+
+        if not error_exp:
+            raise ShellCmdFail(command)
+
+        if (output_exp is not None) and (bool(output) != bool(output_exp)):
+            # TODO: raise exception
+            self._log.debug("Output error")
+
+        return output, exit_status
+
+    def _exec_shell_command(self, command, timeout, retry_count, retry_delay):
+        """
+        """
+        try:
+            channel = self._client._transport.open_session()
+        except socket.error:
+            if retry_count == 0:
+                raise
+
+            self._reconnect(retry_count, retry_delay)
+            channel = self._client.transport.open_session()
+        channel.set_combine_stderr(True)
+
+        start_time = time.time()
+
+        try:
+            if self._debug:
+                self._log.debug("Executing command: {}".format(command))
+            channel.exec_command(command)
+        except paramiko.SSHException:
+            if not self._client.connected():
+                raise
+            else:
+                self._log.debug("Ignored exception")
+
+        channel_closed = False
+        data_received = ''
+
+        while not channel_closed:
+
+            r, w, x = select.select([channel], [], [], 10)
+
+            if timeout and (time.time() - start_time) > timeout:
+                    raise VaAccessTimedout(data_received, timeout)
+
+            if len(r) > 0:
+                new_data = channel.recv(RECV_BYTES)
+                if len(new_data) > 0:
+                    decoded_data = new_data.decode('utf-8')
+                    data_received += decoded_data
+                else:
+                    channel_closed = True
+            elif channel.exit_status_ready():
+                channel_closed = True
+
+        while not channel.exit_status_ready():
+            if timeout and (time.time() - start_time) > timeout:
+                    raise VaAccessTimedout(data_received, timeout=timeout)
+            else:
+                time.sleep(0.5)
+
+        exit_status = channel.recv_exit_status()
+        channel.close()
+
+        return data_received, exit_status
+
+    def download(self, remote=None, local=None):
+        """
+        Method to download a remote file to the local path.
+
+        Kwargs:
+            :remote (str): absolute path of the remote file
+            :local (str): absolute path of the local file
+        """
+        if not self._client.connected():
+            self._client.connect()
+        transport = self._client.get_transport()
+        sftp = transport.open_sftp_client()
+        sftp.get(remote, local)
+        sftp.close()
+        self._client.disconnect()
+
+    def upload(self, remote=None, local=None):
+        """
+        Method to upload a local file to the remote path.
+
+        Kwargs:
+            :local (str): absolute path of the local file
+            :remote (str): absolute path of the remote file
+        """
+        transport = self._client.get_transport()
+        sftp = transport.open_sftp_client()
+        sftp.put(local, remote)
+        sftp.close()
+
+    def _reconnect(self, retry_count, retry_delay):
+        """
+        """
+        for count in range(retry_count):
+            try:
+                self._client.connect()
+                return
+            except paramiko.SSHException:
+                time.sleep(retry_delay)
+
+        #TODO: raise Exception
+
+    def _parse_as_lines(self, output):
+        lines = list()
+        if output:
+            for line in output.readlines():
+                lines.append(line.rstrip())
+
+        self.output = lines
+
+    def __del__(self):
+        """
+        close the ssl connection before destroying the instance
+        """
+        self._client.disconnect()
+
+
+
+'''
+ubantu.py
+class Mode(Common):
+    """
+    Model class implements methods to run linux model commands specific to
+    ubuntu or debian distribution.    
+    """
+    pass
+'''
+        
 class Mode(object):
     """
     Model implements the base class for linux product models.
@@ -38,7 +443,9 @@ class Mode(object):
         abs_path = os.path.dirname(os.path.abspath(__file__))
 
         try:
+            #查找ubantu
             mod = find_module(access_class, [abs_path])
+            #导入ubantu
             load_mod = load_module(access_class, mod[0], mod[1], mod[2])
             mode = getattr(load_mod, 'Mode')
         except:
@@ -112,7 +519,8 @@ class ConfigUtil(object):
         """
         self._resource = None
         self._access = None
-
+        
+        
 class Common(ConfigUtil):
     """
     Base class for the linux feature configuration on the remote
