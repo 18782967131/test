4cc3694d6fb6d522880c3b502708da95b4ecc02e
18782967131 <515947075@qq.com>
2017-03-23 15:33:18 +0800

    change pc class

--- automation/vats/PC类.py
+++ automation/vats/PC类.py
@@ -19,12 +19,99 @@ import re
 import os
 import sys
 import time
-
+from imp import find_module, load_module
 from vautils import logger
 from ipaddress import AddressValueError, NetmaskValueError
 from vautils.config.linux import ConfigUtil
 from time import gmtime, strftime
+class Mode(object):
+    """
+    Model implements the base class for linux product models.
+    """
+    @classmethod
+    def get(cls, resource=None):
+        """
+        class method to get the model associated with a specific linux
+        distribution running on the resource vm.
+        """
+        access_class = resource.get_version().lower()
+        abs_path = os.path.dirname(os.path.abspath(__file__))
 
+        try:
+            mod = find_module(access_class, [abs_path])
+            load_mod = load_module(access_class, mod[0], mod[1], mod[2])
+            mode = getattr(load_mod, 'Mode')
+        except:
+            raise
+        else:
+            return mode(resource)
+
+    def __init__(self, resource=None):
+        """
+        Initialize the model object for the linux product.
+        """
+        user = resource.get_user()
+        self._hostname = resource.get_hostname()
+        session = resource.get_terminal_session()
+        self._resource = resource
+        
+        if not session: 
+            self._shell = LinuxShell(
+                host=resource.get_mgmt_ip(),
+                user=user.get('name'),
+                pswd=user.get('password')
+            )
+            resource.set_terminal_session(self._shell)
+        else:
+            self._shell = session 
+
+    def log_command(self, command=None):
+        """
+        Log the command and the role of the vm that executes the command.
+
+        Kwargs:
+            :command (str): cli command executed
+        """
+        logger.info("[{}] {}".format(self._hostname, command))
+
+    def shell(self, cmd=None):
+        """
+        get the shell attribute of the model class.
+        """
+        self.log_command(cmd)
+        return self._shell.exec_command(cmd)
+
+    def __del__(self):
+        """
+        """
+        self._resource.set_terminal_session(None)
+        if self._shell._client.connected():
+            self._shell._client.disconnect()
+        self._shell = None
+        self._resource = None
+
+class ConfigUtil(object):
+    """
+    Feature implements the base class for linux product features.
+    """
+    def __init__(self, resource=None):
+        """
+        Initialize the feature object for linux product.
+
+        Kwargs:
+            :resource (VaLinux Lab object)
+        """
+        self._resource = resource
+        mode = Mode.get(resource)
+        self._access = mode
+
+    def __del__(self):
+        """
+        Custom delete to unlink the references to model and resource
+        objects.
+        """
+        self._resource = None
+        self._access = None
 
 class Common(ConfigUtil):
     """
